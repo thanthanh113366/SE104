c3d70d673c8bd67267acfe345617e828f0d96c9a
hmt <thanthanh113366@gmail.com>
2025-05-04 21:30:01 +0700

    Xóa thông tin nhạy cảm khỏi mã nguồn

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // Cấu hình Firebase từ biến môi trường
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyDUpwKOocg3VOZlyMO2_nfymoNcUjx0YuA",
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "se104-85ce1.firebaseapp.com",
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "se104-85ce1",
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "se104-85ce1.firebasestorage.app",
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "733364175434",
-  appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:733364175434:web:feaab1162c001516939cb3",
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || "G-9LCPER6VFE"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Khởi tạo Firebase
